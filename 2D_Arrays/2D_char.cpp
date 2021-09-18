3e56cbe42c20f933eed9e00ba2401de53e68c5da
Khushi Jain <khushi.jain0704@gmail.com>
2021-09-18 22:22:56 +0530

    day 10

---
+++ 2D_Arrays/2D_char.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+    char numbers[][10]={
+        "one",
+        "two",
+        "three",
+        "four",
+        "forty",
+        "hundred",
+    }
+    return 0;
+}
