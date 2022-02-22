2174bb781b3a868478df1a06b87fa736a6b46b82
Khushi Jain <khushi.jain0704@gmail.com>
2022-02-22 18:00:31 +0530

    rehashing

--- hashing/hashtable.h
+++ hashing/hashtable.h
@@ -1,3 +1,4 @@
+#include <cstring>
 template<typename T>
 class Node{
     public:
@@ -11,13 +12,18 @@ class Node{
         this->value=value;
         next=NULL;
     }
+    ~Node(){
+        if(next!=NULL){
+            delete next;
+        }
+    }
 };
 template<typename T>
 class HashTable{
     Node<T> ** table;
     int cs; // curretn size
     int ts; // total size
-    public:
+    
 
     int hashfun(string key){
         int power=1;
@@ -29,6 +35,38 @@ class HashTable{
         return idx;
     }
 
+    void rehash(){
+        // save the ptr to the oldtable and we will do insertions in the new table
+       
+        Node<T> **oldTable = table;
+        // save its size
+           int oldTs = ts;
+            ts = 2*ts+1;
+        table = new Node<T>*[ts]; // you should make it prime
+        for(int i-0;i<ts;i++){
+            table[i]=NULL;
+        }
+        // copy elements from the old table to
+        // the new table
+        for(int i=0;i<ts;i++){
+            Node<T> *temp= oldTable[i];
+            while(temp!=NULL){
+                string key = temp->key;
+                T value = temp->value;
+                // happens in the new table
+                insert(key,value);
+                temp=temp->next;
+            }
+            // now delete the old table enteries
+            if(oldTable[i]){
+                delete oldTable[i];
+            }
+        }
+        //its a static/local variabble so it will destrpy
+        // when this funtion call is over
+        delete [] oldTable;
+    }
+    public:
     HashTable(int default_size = 7){
         ts=default_size;
 // a table pointing to an array
