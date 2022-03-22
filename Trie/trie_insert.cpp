f812fa511adf20632fd755c478a2b1661e23e536
khushi <khushi.jain0704@gmail.com>
2022-03-22 16:10:20 +0530

    trie completed

---
+++ Trie/trie_insert.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <unordered_map>
+using namespace std;\
+class Trie;
+class Node{
+    char data;
+    unordered_map<char,Node*> m;
+    bool isTerminal;
+    public:
+    Node(int data){
+        this->data = data;
+        isTerminal = false;
+    }
+    friend class Trie;
+};
+class Trie{
+    Node*root;
+    Trie(){
+        root= new Node('\0');
+    }
+    //insertion
+    void insert(string word){
+        Node *temp=root;
+        for(char ch:word){
+            if(temp->m.count(ch)==0){
+                // it doesnt contain the char , so we'll insert
+                Node *n = new Node(ch);
+                temp->m[ch] = n;
+            }
+            temp=temp->m[ch];
+        }
+        temp->isTerminal=true;
+    }
+};
